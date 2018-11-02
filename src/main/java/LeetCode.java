49473e3feca55e18c6328b30d11be36cab84f3fe
Raja <39787263+crajaramachandran@users.noreply.github.com>
2018-11-02 01:05:11 -0400

    My first command line project

---
+++ src/main/java/LeetCode.java
@@ -0,0 +1,14 @@
+public class LeetCode {
+
+    public static void main(String args[]){
+        Fn fn = new Fn();
+        System.out.println(fn.add(1,2));
+    }
+}
+
+class Fn{
+
+    int add(int a,int b){
+        return a + b;
+    }
+}
